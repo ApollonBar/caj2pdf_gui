7301ca5bdbb880153a249e6000f043808b087c8b
ElonH <846131012@qq.com>
2019-02-28 16:22:41 +0800

    Specific the feature of outlines

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from os import replace,path
-from sys import argv,exit
+from os import path
+from shutil import move
+from sys import argv, exit
 from traceback import format_exc
 
 from PyQt5.QtWidgets import QApplication, QFileDialog, QMainWindow
@@ -15,7 +16,8 @@ class myWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.show()
 
-        self.file_open_button.clicked.connect(self.open_caj)
+        self.caj_file_open_button.clicked.connect(self.open_caj)
+        self.pdf_file_open_button.clicked.connect(self.open_pdf)
         self.show_button.clicked.connect(self.show_caj)
         self.convert.clicked.connect(self.convert_caj)
         self.outlines.clicked.connect(self.outlines_caj)
@@ -23,25 +25,24 @@ class myWindow(QMainWindow, Ui_MainWindow):
     def log(self, s):
         self.logger.addItem(s)
 
-    def check_file_exist(self):
-        txt = self.line_address.text()
-        if path.exists(txt) and path.isfile(txt):
+    def check_file_exist(self, txt, suffix):
+        if path.exists(txt) and path.isfile(txt) and txt[-4:] == suffix:
             return True
-        self.log('caj文件地址错误')
+        self.log(suffix + '文件地址错误')
         return False
 
     def getDefaultOutput(self):
-        txt = self.line_address.text()
+        txt = self.caj_address.text()
         return txt[:-4] + '.pdf'
 
     def show_caj(self):
         self.logger.clear()
         self.log('show')
-        if self.check_file_exist() is False:
+        if self.check_file_exist(self.caj_address.text(), '.caj') is False:
             self.log('--------')
             return
-        caj = CAJParser(self.line_address.text())
-        self.log('File: %s' % self.line_address.text())
+        caj = CAJParser(self.caj_address.text())
+        self.log('File: %s' % self.caj_address.text())
         self.log('Type: %s' % caj.format)
         self.log('Page count: %s' % caj.page_num)
         self.log('Outlines count: %s' % caj.toc_num)
@@ -49,11 +50,11 @@ class myWindow(QMainWindow, Ui_MainWindow):
 
     def convert_caj(self):
         self.log('convert')
-        if self.check_file_exist() is False:
+        if self.check_file_exist(self.caj_address.text(), '.caj') is False:
             self.log('--------')
             return
         try:
-            caj = CAJParser(self.line_address.text())
+            caj = CAJParser(self.caj_address.text())
             caj.convert(self.getDefaultOutput())
             self.log('output: %s' % self.getDefaultOutput())
         except Exception as e:
@@ -63,15 +64,18 @@ class myWindow(QMainWindow, Ui_MainWindow):
 
     def outlines_caj(self):
         self.log('outlines')
-        if self.check_file_exist() is False:
+        if self.check_file_exist(self.caj_address.text(), '.caj') is False:
+            self.log('--------')
+            return
+        if self.check_file_exist(self.pdf_address.text(), '.pdf') is False:
             self.log('--------')
             return
         try:
-            caj = CAJParser(self.line_address.text())
+            caj = CAJParser(self.caj_address.text())
             toc = caj.get_toc()
-            add_outlines(toc, self.getDefaultOutput(), "tmp.pdf")
-            replace("tmp.pdf", self.getDefaultOutput())
-            self.log('output: %s' % self.getDefaultOutput())
+            add_outlines(toc, self.pdf_address.text(), "tmp.pdf")
+            move("tmp.pdf", self.pdf_address.text())
+            self.log('output: %s' % self.pdf_address.text())
         except Exception as e:
             self.log('Exception: %s' % e)
 
@@ -79,7 +83,11 @@ class myWindow(QMainWindow, Ui_MainWindow):
 
     def open_caj(self):
         file_path, _ = QFileDialog.getOpenFileName(self, "选择caj文件", '.', 'caj file(*.caj)')
-        self.line_address.setText(file_path)
+        self.caj_address.setText(file_path)
+
+    def open_pdf(self):
+        file_path, _ = QFileDialog.getOpenFileName(self, "选择需要添加outlines的pdf文件", '.', 'pdf file(*.pdf)')
+        self.pdf_address.setText(file_path)
 
 
 if __name__ == '__main__':
